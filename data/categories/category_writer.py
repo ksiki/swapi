9c84aa89bd1d35a55b076f5630cd46800fcb249b
j-kolyasha <j.kolyasha@gmail.com>
2026-01-08 21:33:42 +0300

    update category_writer.py

--- data/categories/category_writer.py
+++ data/categories/category_writer.py
@@ -21,7 +21,7 @@ class CategoryWriter(ABC):
         raise NotImplementedError
 
     def _write_to_excel(self, data: list[dict], category: str):
-        df = pd.DataFrame(data)
+        df: pd.DataFrame = pd.DataFrame(data)
         with pd.ExcelWriter(self.__path, engine=self.ENGINE) as writer:
             df.to_excel(writer, sheet_name=category, index=False)
 
